7db72a122a6929a0b10cdebf4f772c985e2e88f7
tomires <me@tomires.eu>
2021-06-18 09:28:07 +0200

    Add line renderer that tracks camera position in top down

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,4 +1,6 @@
 public class Constants
 {
     public const string EnvironmentScene = "Environment";
+    public const string PovCameraName = "PoVCamera";
+    public const float LineRefreshPeriod = 0.1f;
 }
--- Assets/Scripts/Supervisor.cs
+++ Assets/Scripts/Supervisor.cs
@@ -4,7 +4,10 @@ using Mirror.Discovery;
 using System.Net;
 using System.Collections.Generic;
 using System;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
+[RequireComponent(typeof(LineRenderer))]
 public class Supervisor : MonoSingleton<Supervisor>
 {
     [Header("Device Selection")]
@@ -18,13 +21,35 @@ public class Supervisor : MonoSingleton<Supervisor>
     private NetworkManager networkManager;
     private NetworkDiscovery networkDiscovery;
     private List<IPEndPoint> discoveredServers = new List<IPEndPoint>();
+    private LineRenderer lineRenderer;
+    private Transform povCamera;
+    private Coroutine drawingCoroutine;
 
     void Awake()
     {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.sceneLoaded += (scene, loadSceneMode) => OnSceneLoad(scene);
+
+        lineRenderer = GetComponent<LineRenderer>();
         networkManager = FindObjectOfType<NetworkManager>();
         networkDiscovery = FindObjectOfType<NetworkDiscovery>();
         networkDiscovery.OnServerFound.AddListener(OnServerFound);
         networkDiscovery.StartDiscovery();
+    }
+
+    private IEnumerator DrawLine()
+    {
+        while(true)
+        {
+            var position = lineRenderer.positionCount++;
+            lineRenderer.SetPosition(position, povCamera.position);
+            yield return new WaitForSeconds(Constants.LineRefreshPeriod);
+        }
+    }
+
+    private void ResetLine()
+    {
+        lineRenderer.positionCount = 0;
     }
 
     private void OnServerFound(ServerResponse response)
@@ -42,5 +67,13 @@ public class Supervisor : MonoSingleton<Supervisor>
         networkManager.StartClient(uri);
         deviceSelectionPanel.SetActive(false);
         supervisorPanel.SetActive(true);
+    }
+
+    private void OnSceneLoad(Scene scene)
+    {
+        if (scene.name != Constants.EnvironmentScene) return;
+
+        povCamera = GameObject.Find(Constants.PovCameraName).transform;
+        drawingCoroutine = StartCoroutine(DrawLine());
     }
 }
