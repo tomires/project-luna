43b5f3c9c732841e6198c058c168ad4a947faba4
tomires <me@tomires.eu>
2021-06-19 19:59:04 +0200

    Map POV camera onto VR camera

--- Assets/Scripts/TestSubject.cs
+++ Assets/Scripts/TestSubject.cs
@@ -3,19 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 using Mirror.Discovery;
+using UnityEngine.SceneManagement;
 
 public class TestSubject : MonoSingleton<TestSubject>
 {
     private NetworkManager networkManager;
     private NetworkDiscovery networkDiscovery;
+    private GameObject vrCamera;
+    private GameObject povCamera;
 
     void Start()
     {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.sceneLoaded += (scene, loadSceneMode) => OnSceneLoad(scene);
+
         networkDiscovery = FindObjectOfType<NetworkDiscovery>();
         networkDiscovery.StartDiscovery();
 
         networkManager = FindObjectOfType<NetworkManager>();
         networkManager.StartHost();
         networkDiscovery.AdvertiseServer();
+
+        /* VR Camera is the only camera we have with AudioListener attached to it */
+        vrCamera = FindObjectOfType<AudioListener>().gameObject;
+    }
+
+    private void OnSceneLoad(Scene scene)
+    {
+        if (scene.name != Constants.EnvironmentScene) return;
+        StartCoroutine(SynchronizePovCamera());
+    }
+
+    private IEnumerator SynchronizePovCamera()
+    {
+        yield return new WaitForSeconds(1.0f);
+        povCamera = GameObject.Find(Constants.PovCameraName);
+
+        while (true)
+        {
+            povCamera.transform.position = vrCamera.transform.position;
+            povCamera.transform.rotation = vrCamera.transform.rotation;
+            yield return null;
+        }
     }
 }
